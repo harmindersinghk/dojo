22a014a53322b258032d6bc6e42413674599bdf5
Harminder Singh <P94279@MacBook-Pro-6.home>
2018-12-21 12:31:59 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const server = require('./lib/server')
 const pkg = require('./package.json')
+
+var appzip = require('appmetrics-zipkin')({
+    host: '51.141.29.230',
+    port: 9401,
+    serviceName:pkg.name,
+    sampleRate: 1.0
+  });
+
+const server = require('./lib/server')
 const PORT = process.env.PORT || 8001
 const log = require('./lib/log')
 
